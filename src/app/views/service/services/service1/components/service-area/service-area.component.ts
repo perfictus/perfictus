6293dbea609041620936ee102663a801fdae0fd0
vijai <vijaijesudoss@gmail.com>
2026-01-07 23:59:20 +0530

    Updated UI and fixes before go-live

--- src/app/views/service/services/service1/components/service-area/service-area.component.ts
+++ src/app/views/service/services/service1/components/service-area/service-area.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BreadcrumbComponent } from '../../../../../../components/breadcrumb/breadcrumb.component';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-service-area',
-  imports: [RouterLink],
+  imports: [BreadcrumbComponent, NgbAccordionModule,RouterLink],
   templateUrl: './service-area.component.html',
   styles: ``
 })
 export class ServiceAreaComponent {
+  scrollTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+activeAccordion: string = 'panel-0';
 
+  onPanelShow(panelId: string) {
+    this.activeAccordion = panelId;
+  }
 }
