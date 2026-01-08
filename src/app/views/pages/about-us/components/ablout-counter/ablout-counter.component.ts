94c7915e81e51d7d02479a92f5d58a1417425b6f
vijai <vijaijesudoss@gmail.com>
2026-01-09 02:53:26 +0530

    Updated UI / new changes

--- src/app/views/pages/about-us/components/ablout-counter/ablout-counter.component.ts
+++ src/app/views/pages/about-us/components/ablout-counter/ablout-counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ablout-counter',
@@ -6,6 +6,38 @@ import { Component } from '@angular/core';
   templateUrl: './ablout-counter.component.html',
   styles: ``
 })
-export class AbloutCounterComponent {
+export class AbloutCounterComponent implements OnInit {
 
+  projectCount = 0;
+  revenueCount = 0;
+  clientCount = 0;
+
+  ngOnInit(): void {
+    this.animateCounter('projectCount', 114, 2000);
+    this.animateCounter('revenueCount', 3, 1500);
+    this.animateCounter('clientCount', 57, 2000);
+  }
+
+  animateCounter(
+    property: 'projectCount' | 'revenueCount' | 'clientCount',
+    target: number,
+    duration: number
+  ) {
+    const start = 0;
+    const incrementTime = 20;
+    const steps = duration / incrementTime;
+    const increment = target / steps;
+
+    let current = start;
+
+    const timer = setInterval(() => {
+      current += increment;
+      this[property] = Math.floor(current);
+
+      if (current >= target) {
+        this[property] = target;
+        clearInterval(timer);
+      }
+    }, incrementTime);
+  }
 }
